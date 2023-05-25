7a2e850363e163356a616ea1c0718d0dfc019896
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-25 16:48:37 -0300

    Update Exercício 12.c

--- Ponteiro/Exercício 12.c
+++ Ponteiro/Exercício 12.c
@@ -1,3 +1,8 @@
+//A alocação dinâmica de memória permite trabalhar com matrizes de formas diferentes, a saber:
+//• Utilizando um ponteiro simples.
+//• Utilizando um arranjo de ponteiro.
+//• Utilizando um ponteiro para ponteiro
+
 #include <stdio.h>
 #include <stdlib.h>
 
