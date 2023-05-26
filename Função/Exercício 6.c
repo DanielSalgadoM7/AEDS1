dbcf6768fa91effe51db69cabfc6b3df2f2c3a9a
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-26 17:27:32 -0300

    Update Exercício 6.c

--- Função/Exercício 6.c
+++ Função/Exercício 6.c
@@ -2,12 +2,19 @@
 
 #include <stdio.h>
 
+int calculaFatorial(int num, int fatorial){
+  for(int i=1;i<=num;i++){
+    fatorial *= i;
+  }
+  return printf("O fatorial do número %d é %d", num, fatorial);
+}
+
 int main(void) {
-  int num;
+  int num, fatorial = 1;
 
   printf("Digite o número que deseja saber o fatorial: ");
   scanf("%d", &num);
 
-  calculaFatorial
+  calculaFatorial(num, fatorial);
   return 0;
 }
