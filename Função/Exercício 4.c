642c6df6be23609a7020ba09882ff99320c7a423
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-26 00:09:47 -0300

    Create Exercício 4.c

---
+++ Função/Exercício 4.c
@@ -0,0 +1,34 @@
+/*Escreva um procedimento que recebe 3 valores reais X, Y e Z e que verifique se esses valores podem ser os comprimentos dos lados de um triângulo e, neste caso, exibe qual é o tipo de triângulo formado. 
+Para que X, Y e Z formem um triângulo é necessário que a seguinte propriedade seja satisfeita: o comprimento de cada lado de um triângulo é menor
+do que a soma do comprimento dos outros dois lados. O procedimento deve identificar o tipo de triângulo formado observando as seguintes definições:
+- Triângulo Equilátero: os comprimentos dos 3 lados são iguais;
+- Triângulo Isósceles: os comprimentos de pelo menos 2 lados são iguais.
+- Triângulo Escaleno: os comprimentos dos 3 lados são diferentes.
+Faça um programa que leia um número indeterminado de triângulos (valores dos 3 lados) e para cada triângulo, acione o procedimento*/
+
+#include <stdio.h>
+
+void calculaTriangulo(int n1, int n2, int n3){
+
+  printf("Digite o lado de cada triângulo");
+  scanf("%d %d %d", &n1, &n2, &n3);
+  
+  if(n1 == n2 && n1 == n3){
+    printf("O triângulo é equilátero");
+  }else if(n1 != n2 && n2 == n3 || n1 == n2 && n1 != n3){
+    printf("O triângulo é isósceles");
+  }else{
+    printf("O triângulo é escaleno");
+  }
+}
+
+int main(){
+  int n1, n2, n3, qnt;
+
+  printf("Quantos triângulos deseja ler? ");
+  scanf("%d", &qnt);
+
+  for(int i=0;i<qnt;i++){
+    calculaTriangulo(n1, n2, n3);
+  }
+}
