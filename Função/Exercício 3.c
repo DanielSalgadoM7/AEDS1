fa6b9b49351ab240c82b3b098d007667d8e3b7f1
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-25 23:49:30 -0300

    Create Exercício 3.c

---
+++ Função/Exercício 3.c
@@ -0,0 +1,15 @@
+//Faça um procedimento que recebe 3 valores inteiros por parâmetro e os exiba em ordem crescente. 
+//Faça um programa que leia N conjuntos de 3 valores e acione o procedimento para cada conjunto. (N deve ser lido do teclado)
+
+#include <stdio.h>
+
+int main(){
+  int n1, n2, n3, qnt;
+
+  printf("Quantos conjuntos de 3 números você deseja digitar? ");
+  scanf("%d", &qnt);
+
+  for(int i=0;i<qnt;i++){
+    
+  }
+}
