50d67fcda4fdd01af746bff4ab29907e43ef5812
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-26 17:20:58 -0300

    Create Exercício 6.c

---
+++ Função/Exercício 6.c
@@ -0,0 +1,13 @@
+//Escreva uma função que receba um número inteiro e retorne o fatorial deste número.
+
+#include <stdio.h>
+
+int main(void) {
+  int num;
+
+  printf("Digite o número que deseja saber o fatorial: ");
+  scanf("%d", &num);
+
+  calculaFatorial
+  return 0;
+}
