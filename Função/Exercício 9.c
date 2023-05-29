fbeb121673b7fb8d89368f2856c5d62873c4eb36
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-29 17:49:39 -0300

    Update Exercício 9.c

--- Função/Exercício 9.c
+++ Função/Exercício 9.c
@@ -4,22 +4,20 @@ S = 2/4 + 5/5 + 10/6 + 17/7 + 26/8 + ... +(n^2+1)/(n+3). Faça um programa que l
 #include <stdio.h>
 #include <math.h>
 
-float calculaSomatorio(int qnt, float S[]){
+float calculaSomatorio(int qnt, float *S){
   for(int i=1;i<=qnt;i++){
-  S[i] += (float)(pow(i,2) + 1)/(i+3);
+  *S += (float)(pow(i,2) + 1)/(i+3);
   }
+  return *S;
 }
-
 
 int main(void) {
   int qnt;
-  float somatorio = 0;
+  float *S = 0;
 
   printf("Digite o número: ");
   scanf("%d", &qnt);
 
-  calculaSomatorio(qnt, &somatorio);
-
-  printf("O somatório é: %.2f", somatorio);
+  printf("O somatório é: %.2f",calculaSomatorio(qnt, &S));
   return 0;
 }
