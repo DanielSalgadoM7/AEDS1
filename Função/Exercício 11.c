45c053cc4acf0b990431e3ec2da8a652bb04c066
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-30 14:42:41 -0300

    Create Exercício 11.c

---
+++ Função/Exercício 11.c
@@ -0,0 +1,8 @@
+/*Faça uma função que recebe a idade de um nadador por parâmetro e retorna a categoria desse nadador de acordo com a tabela abaixo.
+Idade (anos) Categoria
+5 a 7 F
+8 a 10 E
+11 a 13 D
+14 a 15 C
+16 a 17 B
+Acima de 18 A Faça um programa que lê a idade de um nadador e imprime a categoria retornada pela função*/
