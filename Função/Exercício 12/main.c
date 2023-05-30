cbc0b268685dbbaa6b802fe916c97412bc90d0f7
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-30 16:01:16 -0300

    Update main.c

--- Função/Exercício 12/main.c
+++ Função/Exercício 12/main.c
@@ -9,19 +9,35 @@ float minArray(int n, int v[]);
 Recebe o array e seu tamanho. Retorna o menor elemento do array.
 Crie um programa para testar cada um dos métodos da biblioteca*/
 
-
 #include "arrays.h"
 #include <stdio.h>
 
 int main(void) {
-  int n, vet[n];
+  int n;
 
   printf("Digite a quantidade de elementos do vetor: ");
   scanf("%d", &n);
 
+  int v[n];
+  int menor = 0, maior = 0;
+  float media = 0;
+  
   printf("Digite os elementos do vetor: ");
   for(int i=0;i<n;i++){
-    scanf("\n%d",&vet[i]);
+    scanf("\n%d", &v[i]);
   }
+
+  printArray(n, v); 
+
+  printf("\n");
+  
+  media = mediaArray(n, v);
+  printf("\nA media do vetor é: %.2f \n", media);
+  
+  maior =  maxArray(n, v);
+  printf("\nO maior número do vetor é: %d \n", maior);
+
+  menor =  minArray(n, v);
+  printf("\nO menor número do vetor é %d \n", menor);
   return 0;
 }
