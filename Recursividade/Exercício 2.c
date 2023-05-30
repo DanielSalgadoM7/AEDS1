32e69f80bf06b581b58c974bde78970ff277a531
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-30 17:07:36 -0300

    Update Exercício 2.c

--- Recursividade/Exercício 2.c
+++ Recursividade/Exercício 2.c
@@ -1,1 +1,23 @@
 //Escreva uma função que receba um número inteiro e retorne o fatorial deste número usando a abordagem recursiva
+
+#include <stdio.h>
+
+int calculaFatorial(n){
+  if(n==1 || n==0){
+    return 1;
+  }else{
+    return n * calculaFatorial(n-1);
+  }
+}
+
+int main(void) {
+  int n, fatorial = 1;
+
+  printf("Digite um número inteiro: ");
+  scanf("%d", &n);
+
+  fatorial = calculaFatorial(n);
+
+  printf("O fatorial de %d é: %d", n, fatorial);
+  return 0;
+}
