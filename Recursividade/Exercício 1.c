6dc01090c3fda74ebed5dd941243cfd4ae83650f
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-30 16:52:01 -0300

    Update Exercício 1.c

--- Recursividade/Exercício 1.c
+++ Recursividade/Exercício 1.c
@@ -1,1 +1,22 @@
+//Escreva uma função recursiva que dado dois números 𝑥 e 𝑛, calcula o valor 𝑥 ∗ 𝑛 (𝑥 multiplicado por 𝑛). Não use o operador de multiplicação.
 
+#include <stdio.h>
+
+int multiplicaNumero(x, n){
+  if(n==1){
+    return x;
+  }else{
+    return x + multiplicaNumero(x, n-1);
+  }
+}
+
+int main(void) {
+  int x, n, resultado;
+
+  printf("Digite o valor de X e de N: ");
+  scanf("%d %d", &x, &n);
+
+  resultado = multiplicaNumero(x, n);
+  printf("O valor de %d * %d é: %d", x, n, resultado);
+  return 0;
+}
