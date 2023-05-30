7a2eaaaf47ab28c753fac99046a284d14fba4a9b
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-30 19:22:54 -0300

    Update Exercício 4.c

--- Recursividade/Exercício 4.c
+++ Recursividade/Exercício 4.c
@@ -1,1 +1,21 @@
+//Escreva uma função recursiva que calcula a soma de todos os elementos de um arranjo de números reais
 
+#include <stdio.h>
+
+int calculaSoma(int n, int vet[]){
+  int soma;
+  if(n==0){
+    return 0;
+  }else{
+    return vet[n-1] + calculaSoma(n -1,vet);
+  }
+}
+
+int main(void) {
+  int n = 5, soma;
+  int vet[] = {2,3,4,3,-2};
+  
+  soma = calculaSoma(n, vet);
+  printf("A soma de todos os elementos do vetor é: %d", soma);
+  return 0;
+}
