510d280c718c8dfa4e84db6fcd6c6782a883a5a6
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-05-30 19:55:26 -0300

    Update Exercício 5.c

--- Recursividade/Exercício 5.c
+++ Recursividade/Exercício 5.c
@@ -1,1 +1,28 @@
+// Escreva uma função recursiva que determina quantas vezes um número inteiro x presente em vetor de números inteiros
 
+#include <stdio.h>
+
+int contador(int tamanho, int v[tamanho], int numero) {
+  if (tamanho == 0) {
+    return 0;
+  }
+
+  //o contador acontece no +1 quando o número repete
+  else if (v[tamanho] == numero) {
+    return 1 + contador(tamanho - 1, v, numero);
+  }
+
+  //o contador fica normal
+  else {
+    return contador(tamanho - 1, v, numero);
+  }
+}
+
+int main(void) {
+  int vetor[10] = {2, 2, 2, 5, 6, 7, 8, 9, 2, 1};
+  //declaro o contador já dizendo que o tamanho é 10 e que quero o 2 como número repetido
+  int cont = contador(10, vetor, 2);
+
+  printf("\nO numero 2 aparece %d vezes no vetor\n", cont + 1);
+  return 0;
+}
