7939112727f2ecabd5a7807b6daa4cf9682be9dd
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-06-07 15:32:42 -0300

    Update Exercício 1.c

--- String/Exercício 1.c
+++ String/Exercício 1.c
@@ -1,2 +1,17 @@
 //Ler uma string de no máximo 50 caracteres e contar quantas letras A essa palavra possui.
 
+#include <stdio.h>
+
+int main(void) {
+  int contador = 0;
+  char texto[50];
+  scanf(" %[^\n]", texto);
+
+  for(int i=0;i<50;i++){
+    if(texto[i]=='a'){
+      contador++;
+    }
+  }
+  printf("%d letras A", contador);
+  return 0;
+}
