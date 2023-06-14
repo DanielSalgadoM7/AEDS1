1cce0ce4f992252174292186891bc5439845ccef
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-06-14 11:33:38 -0300

    Update Exercício 2.c

--- String/Exercício 2.c
+++ String/Exercício 2.c
@@ -1,1 +1,17 @@
-//Ler uma string de no máximo 50 caracteres e indicar quais as posições da letra A nessa palavra
+//Ler uma string de no máximo 50 caracteres e indicar quais as posições da letra A nessa palavra.
+
+#include <stdio.h>
+
+int main(void) {
+  char texto[49];
+  printf("Digite um texto: ");
+  scanf("%[^\n]", texto);
+
+  printf("A letra A aparece nas posições: ");
+  for(int i=0;i<50;i++){
+    if(texto[i]=='A'){
+      printf("%d \t", i);
+    }
+  }
+  return 0;
+}
