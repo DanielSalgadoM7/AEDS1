ab542b440d88cb6943b8cb8b4751e6248ad8e3ad
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-06-14 11:34:24 -0300

    Update Exercício 3.c

--- String/Exercício 3.c
+++ String/Exercício 3.c
@@ -1,1 +1,22 @@
+//Ler uma string de no máximo 50 caracteres e em seguida um caractere, mostrar quais as posições esse caractere aparece na string lida e quantas vezes ele apareceu.
 
+#include <stdio.h>
+
+int main(void) {
+  char texto[49], letra;
+  int contador = 0;
+  printf("Digite um texto: ");
+  scanf("%[^\n]", texto);
+
+  printf("Digite um caractere: ");
+  scanf(" %c", &letra);
+
+  for(int i=0;i<50;i++){
+    if(texto[i] == letra){
+      printf("%d\t", i);
+      contador++;
+    }
+  }
+  printf("\nO caracter %c apareceu %d vezes no texto %s !", letra, contador, texto);
+  return 0;
+}
