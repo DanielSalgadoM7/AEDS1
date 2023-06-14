5d83defba624cebc0a399f954987e588d69d8cdc
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-06-14 11:38:09 -0300

    Update Exercício 8.c

--- String/Exercício 8.c
+++ String/Exercício 8.c
@@ -1,1 +1,26 @@
+//Ler uma string de no máximo 50 caracteres e retire dessa string todos os espaços em branco. Sem utilize string auxiliar.
 
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void) {
+  char texto1[50];
+  printf("Digite um texto: ");
+  scanf("%[^\n]", texto1);
+
+  int tamanho = strlen(texto1);
+
+  int j=0;
+  int i=0;
+  for(i=0;i<tamanho;i++){
+    if(!(isspace(texto1[i]))){
+      texto1[j] = texto1[i];
+      j++;
+    }
+  }
+  texto1[j] = '\0';
+
+  printf("%s", texto1);
+  return 0;
+}
