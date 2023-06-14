d48b32da8eba5c54c989280f19346decef75e352
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2023-06-14 11:35:46 -0300

    Update Exercício 5.c

--- String/Exercício 5.c
+++ String/Exercício 5.c
@@ -1,1 +1,30 @@
+//Ler uma string de no máximo 50 caracteres e mostrar quantas letras possui e quantos caracteres são números e quantos não são nem números nem letras.
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void) {
+  char texto[50];
+  int contadorNum = 0;
+  int contadorChar = 0;
+  int contadorLetra = 0;
+  
+  printf("Digite um texto: ");
+  scanf("%[^\n]", texto);
+
+  int tamanho = strlen(texto);
+
+  for(int i=0;i<tamanho;i++){
+    if(isdigit(texto[i])){
+      contadorNum++;
+    }else if(isalpha(texto[i])){
+      contadorLetra++;
+    }else if(!(isalnum(texto[i]))) {
+      contadorChar++;
+    }
+  }
+
+  printf("O texto %s tem %d letras. Sendo %d números e %d caracteres diferentes de números e letras!", texto, contadorLetra, contadorNum, contadorChar - 1);
+  return 0;
+}
