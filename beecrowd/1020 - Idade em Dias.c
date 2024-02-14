527f179a8856d145ed109811c0c772f26c7388dd
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-02-14 12:51:47 -0300

    Solução do problema 1020 - Idade em Dias.c

--- beecrowd/1020 - Idade em Dias.c
+++ beecrowd/1020 - Idade em Dias.c
@@ -2,7 +2,7 @@
 
 Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. 
 Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
-
+ 
 Entrada
 O arquivo de entrada contém um valor inteiro.
 
