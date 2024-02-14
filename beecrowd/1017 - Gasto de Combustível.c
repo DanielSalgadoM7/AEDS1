02d7e3f6334a240e7cf2f2a423b076115857110d
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-02-14 12:48:53 -0300

    Solução do problema 1017 - Gasto de Combustível.c

--- beecrowd/1017 - Gasto de Combustível.c
+++ beecrowd/1017 - Gasto de Combustível.c
@@ -2,7 +2,7 @@
 Para isso, ele gostaria que você o auxiliasse através de um simples programa. 
 Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante a mesma (em km/h). 
 Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. Mostre o valor com 3 casas decimais após o ponto.
-
+ 
 Entrada
 O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é a velocidade média durante a mesma (em km/h).
 
