91f7acf6dde26c33346699d638e44661371f62a1
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-02-14 12:15:35 -0300

    Solução do problema 1004 - Produto Simples.c

--- beecrowd/1004 - Produto Simples.c
+++ beecrowd/1004 - Produto Simples.c
@@ -5,6 +5,7 @@ O arquivo de entrada contém 2 valores inteiros.
 
 Saída
 Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. 
+
 Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.*/
 
 #include <stdio.h>
