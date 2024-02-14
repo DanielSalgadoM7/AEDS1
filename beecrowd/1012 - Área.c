d3b03fe6c2d6b9a291c23b0efce94a48ce71acf6
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-02-14 12:24:56 -0300

    Solução do problema 1012 - Área.c

--- beecrowd/1012 - Área.c
+++ beecrowd/1012 - Área.c
@@ -4,6 +4,7 @@ b) a área do círculo de raio C. (pi = 3.14159)
 c) a área do trapézio que tem A e B por bases e C por altura.
 d) a área do quadrado que tem lado B.
 e) a área do retângulo que tem lados A e B.
+
 Entrada
 O arquivo de entrada contém três valores com um dígito após o ponto decimal.
 
