fd7b886e175590b238b4b2ebf8782e9b4786dea2
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-02-15 21:48:32 -0300

    Solução do problema 1042 - Sort Simples.c

--- beecrowd/1042 - Sort Simples.c
+++ beecrowd/1042 - Sort Simples.c
@@ -6,3 +6,26 @@ A entrada contem três números inteiros.
 Saída
 Imprima a saída conforme foi especificado.*/
 
+#include <stdio.h>
+
+int main() {
+  int A, B, C;
+
+  scanf("%d %d %d", &A, &B, &C);
+
+  if(A<B && B<C){
+    printf("%d\n%d\n%d\n", A, B, C);
+  }else if(B<A && A<C){
+    printf("%d\n%d\n%d\n", B, A, C);
+  }else if(C<A && A<B){
+    printf("%d\n%d\n%d\n", C, A, B);
+  }else if(C<B && B<A){
+    printf("%d\n%d\n%d\n", C, B, A);
+  }else if(B<C && C<A){
+    printf("%d\n%d\n%d\n", B, C, A);
+  }else{
+    printf("%d\n%d\n%d\n", A, C, B);
+  }
+  printf("\n%d\n%d\n%d\n", A, B, C);
+  return 0;
+}
