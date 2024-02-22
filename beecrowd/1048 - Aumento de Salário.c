b3175676df8ed35b552cfe317c4fa12bc6428cc3
Daniel Salgado <111083548+DanielSalgadoM7@users.noreply.github.com>
2024-02-21 21:58:11 -0300

    Solução do problema 1048 - Aumento de Salário.c

--- beecrowd/1048 - Aumento de Salário.c
+++ beecrowd/1048 - Aumento de Salário.c
@@ -21,3 +21,39 @@ A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
 
 Saída
 Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste (ambos devem ser apresentados com 2 casas decimais) e o percentual de reajuste ganho, conforme exemplo abaixo.*/
+
+#include <stdio.h>
+ 
+int main() {
+    double salario= 0.0, reajuste, novoSalario;
+    
+    scanf("%lf", &salario);
+    
+    if(salario <= 400.00){
+        novoSalario = salario + (salario * 0.15);
+        printf("Novo salario: %.2lf\n", novoSalario);
+        printf("Reajuste ganho: %.2lf\n", novoSalario - salario);
+        printf("Em percentual: 15 %\n");
+    }else if(salario > 400.00 && salario <= 800.00){
+        novoSalario = salario + (salario * 0.12);
+        printf("Novo salario: %.2lf\n", novoSalario);
+        printf("Reajuste ganho: %.2lf\n", novoSalario - salario);
+        printf("Em percentual: 12 %\n");
+    }else if(salario > 800.00 && salario <= 1200.00){
+        novoSalario = salario + (salario * 0.10);
+        printf("Novo salario: %.2lf\n", novoSalario);
+        printf("Reajuste ganho: %.2lf\n", novoSalario - salario);
+        printf("Em percentual: 10 %\n");
+    }else if(salario > 1200.00 && salario <= 2000.00){
+        novoSalario = salario + (salario * 0.07);
+        printf("Novo salario: %.2lf\n", novoSalario);
+        printf("Reajuste ganho: %.2lf\n", novoSalario - salario);
+        printf("Em percentual: 7 %\n");
+    }else{
+        novoSalario = salario + (salario * 0.04);
+        printf("Novo salario: %.2lf\n", novoSalario);
+        printf("Reajuste ganho: %.2lf\n", novoSalario - salario);
+        printf("Em percentual: 4 %\n");
+    }
+    return 0;
+}
